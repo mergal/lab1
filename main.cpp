f88e513fb66e0a9e1cc185a03ce10a5341b68ce1
mergal <225955@student.pwr.edu.pl>
2016-03-29 20:40:40 +0200

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -2,22 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
+
+
 int main(int argc, char **argv)
 {
-	int n;
+	srand(atoi(*(argv+2)));
+	int n=atoi(*(argv+1));
 	drawCharSquare(charSquare(n), n);	
+	
 }
+
+
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square=new char* [n];
+	for(int i=0;i<n;i++)
+		*(square+i)=new char [n];
+	
+	for(int i=0;i<n*n;i++)
+		*(*(square+i%n)+i/n)=rand()%26+97;
 	return square;
 }
 
+
+
 void drawCharSquare(char **square, int n)
 {
+	for(int i=0;i<n*n;i++)
+	(i+1)%n==0?printf("%c\n", *(*(square+i%n)+i/n))
+			:printf("%c ",*(*(square+i%n)+i/n));
 
+	for(int i=0;i<n;i++)
+		delete [] *(square+i);
+	delete [] square;
 }
